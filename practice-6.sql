6a86f76b2a489f36760568bc8fb3f948b8c00908
Pengfei Gao <pengfei.gao@kellogg.northwestern.edu>
2024-01-15 01:16:11 +0000

    complete practice 1-7

--- practice-6.sql
+++ practice-6.sql
@@ -1,5 +1,9 @@
 -- How many reviews are written per neighborhood?
 
+SELECT listings.neighborhood, count(*) FROM listings
+INNER JOIN reviews ON reviews.listing_id = listings.id
+GROUP BY listings.neighborhood;
+
 -- +------------------------+----------+
 -- | Albany Park            | 2557     |
 -- | Archer Heights         | 541      |
